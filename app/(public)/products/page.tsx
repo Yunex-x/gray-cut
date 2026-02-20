a67b2781a52d65ec7af5ea40e61f9df04b22a0a1
Yunex-x <yunex.business@gmail.com>
2026-02-20 17:10:03 +0000

    sticky

--- app/(public)/products/page.tsx
+++ app/(public)/products/page.tsx
@@ -17,27 +17,28 @@ export default function Page() {
 
       {/* Two-column container */}
       <div className="w-full bg-[#F6F4F1]">
-        {/* allow full expansion at 2xl so left column can grow: 2xl:max-w-none */}
         <div className="max-w-[1440px] 2xl:max-w-none w-full mx-auto flex flex-col lg:flex-row">
-          {/* LEFT: main content stack (full width on mobile, fixed 1084px on lg+,
-              but expand at 2xl to take available left space up to the aside) */}
+          
+          {/* LEFT */}
           <div className="w-full lg:w-[1084px] 2xl:w-[calc(100vw-356px)]">
             <ProductShowcase />
             <SystemEngulfs />
             <WearingProcess />
             <AfterProtocol />
             <DeliveryProcess />
-             <FooterSection />
-                  <MobileFooter />
           </div>
 
-          {/* RIGHT: ProductConfigurator aside (hidden on small screens, fixed 356px on lg+) */}
+          {/* RIGHT (Sticky) */}
           <aside className="hidden lg:block lg:w-[356px] flex-shrink-0">
-            <div className="lg:py-[120px]">
+            <div className="lg:sticky lg:top-[40px] lg:py-[120px]">
               <ProductConfigurator />
             </div>
           </aside>
+
         </div>
+
+        <FooterSection />
+        <MobileFooter />
       </div>
     </main>
   );
